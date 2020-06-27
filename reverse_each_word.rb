e15a9f1bb602b933635d77900abf492d1f76fe70
Idalis Paz <idalispaz@gmail.com>
2020-06-27 02:52:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++
@@ -1,7 +0,0 @@
-def reverse_each_word(sentence1)
-  sentence1 = "Hello, there and who are you?"
-  string = string.split("")
-  reversed = []
-  string.each  { |r| reversed.unshift(r)}
-  return reversed.join("")
-end 
